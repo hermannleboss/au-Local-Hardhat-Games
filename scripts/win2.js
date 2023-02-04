116f672b252540b9c9a564efe739a6703d1b9517
Hermann EHO <hermannleboss@gmail.com>
2023-02-04 18:56:47 +0000

    feat: win all games
    game 4 is unwinable

--- scripts/win2.js
+++ scripts/win2.js
@@ -1,11 +1,14 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractName = "Game1";
+const gameAddr = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
-
+    const setX= await game.setX(25)
+    const x= setX.wait()
+    const setY= await game.setY(25)
+   const y =  setY.wait()
     // do whatever you need to do to win the game here:
     const tx = await game.win();
 
--- scripts/win5.js
+++ scripts/win5.js
@@ -1,10 +1,15 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractName = "Game1";
+const gameAddr = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+const contractName = "Game5";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+    const giveMeAllowance= await game.giveMeAllowance(100000)
+    const x= giveMeAllowance.wait()
+    const mint= await game.mint(10000)
+    const y =  mint.wait()
 
     // do whatever you need to do to win the game here:
     const tx = await game.win();
